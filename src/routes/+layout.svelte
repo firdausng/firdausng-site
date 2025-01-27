7555df544bf4dd09e61a39224611197764e5312c
Firdaus Kamaruddin <firdauskamaruddin@hotmail.com>
2025-01-27 10:13:36 +0800

    add posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 	import '../app.css';
+    import posthog from 'posthog-js'
     import NavBar from '$lib/components/navBar.svelte';
     import Footer from '$lib/components/footer.svelte';
     import {title} from "$lib/config";
+    import {onMount} from "svelte";
+    import { browser, dev } from '$app/environment';
     
 	let { children } = $props();
+
+    onMount(() => {
+        if (browser && !dev) {
+            posthog.init(
+                'phc_iadMgugWbqGMJ0mOWNWRyQsgEUis7RQvmgcjneaUWBo',
+                {
+                    api_host: 'https://us.i.posthog.com',
+                    person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+                }
+            )
+        }
+        return
+    });
 </script>
 
 <svelte:head>
